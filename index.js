ab93c69ebc5f90d262fc9b299b92f4d48b55f542
MarkVebeer <markveber100@gmail.com>
2025-09-09 16:54:06 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+    ]
+});
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Bot is ready!');
+});
+
+// Login to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN);
